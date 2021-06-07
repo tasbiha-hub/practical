5d341f11e9aaae4ba7bc6f339dceaa0d71d8ea67
tasbiha-hub <80311313+tasbiha-hub@users.noreply.github.com>
2021-06-07 09:19:55 -0700

    Add files via upload

---
+++ program 3/program 3/program 3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstname = "jazz";
+            var lastname = "mosh";
+            var fullname = firstname + " " + lastname;
+            var myfullname = string.Format("my name is{0} {1}", firstname, lastname);
+            var names = new string[3] { "john", "jack", "marry" };
+            var formattednames = string.Join(" ", names);
+            Console.WriteLine(formattednames);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ program 4/program 4/program 4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstname = "jazz";
+            var lastname = "mosh";
+            var fullname = firstname + " " + lastname;
+            var myfullname = string.Format("my name is {0}{1}", firstname, lastname);
+            var names = new string[3] { "john", "jack", "marry" };
+            var formattednames = string.Join(" ", names);
+            var text = "hi john\nlook into the following path\n c:\\folder\\folder 1";
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
